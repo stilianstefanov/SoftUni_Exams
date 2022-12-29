3d057932322fa2620b73f161358fa2960392b899
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-12-29 13:19:25 +0200

    Initial commit

---
+++ C# OOP/C# OOP Exam - 15 August 2021/02.Business Logic/Models/Maps/Map.cs
@@ -0,0 +1,54 @@
+﻿namespace CarRacing.Models.Maps
+{
+    using Racers.Contracts;
+    using Utilities.Messages;
+    using Contracts;
+
+    public class Map : IMap
+    {
+        public string StartRace(IRacer racerOne, IRacer racerTwo)
+        {
+            if (!racerOne.IsAvailable() && !racerTwo.IsAvailable())
+                return OutputMessages.RaceCannotBeCompleted;
+
+            IRacer winningRacer = null;
+            IRacer loserRacer = null;
+
+            if (racerOne.IsAvailable() && !racerTwo.IsAvailable())
+            {
+                winningRacer = racerOne;
+                loserRacer = racerTwo;
+            }
+            else if (!racerOne.IsAvailable() && racerTwo.IsAvailable())
+            {
+                winningRacer = racerTwo;
+                loserRacer = racerOne;
+            }
+
+            if (winningRacer != null)
+            {
+                winningRacer.Race();
+                return string.Format(OutputMessages.OneRacerIsNotAvailable, winningRacer.Username, loserRacer.Username);
+            }
+
+            double racerOneChances = GetWinningChances(racerOne);
+            double racerTwoChances = GetWinningChances(racerTwo);
+
+            racerOne.Race();
+            racerTwo.Race();
+
+            if (racerOneChances > racerTwoChances)
+                winningRacer = racerOne;
+            else
+                winningRacer = racerTwo;
+          
+            return string.Format(OutputMessages.RacerWinsRace, racerOne.Username, racerTwo.Username, winningRacer.Username);
+        }
+
+        private double GetWinningChances(IRacer racer) 
+            => racer.Car.HorsePower * racer.DrivingExperience * GetMultiplier(racer.RacingBehavior);
+
+        private double GetMultiplier(string racingBehavior)
+            => racingBehavior == "strict" ? 1.2 : 1.1;       
+    }
+}
